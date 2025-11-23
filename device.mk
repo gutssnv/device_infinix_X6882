ab65916a6aa4ae66b3d5e1efa3f8c41abab3ceb2
Shirayuki39 <lorddemecrius83@proton.me>
2025-11-23 22:46:41 +0900

    X6882: Add hardware/mediatek/libaedv to SOONG_NAMESPACES
    * Extension of 3cfe4e1852ba6b9fbf8b4d412adab595a82ff207
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -440,6 +440,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
+    hardware/mediatek/libaedv \
     hardware/google/interfaces \
     hardware/google/pixel
 
