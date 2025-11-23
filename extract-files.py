796ddefe0f0185eb063866d3a7174b0792bfe75d
Shirayuki39 <lorddemecrius83@proton.me>
2025-11-23 22:46:41 +0900

    X6882: Add hardware/mediatek/libaedv to SOONG_NAMESPACES
    * Extension of 34c0d2c7b07bbcd9f9f880101b4771c9eda7e2b4
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -23,6 +23,7 @@ namespace_imports = [
     'device/infinix/X6882',
     'hardware/mediatek',
     'hardware/mediatek/libmtkperf_client',
+    'hardware/mediatek/libaedv',
 ]
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
