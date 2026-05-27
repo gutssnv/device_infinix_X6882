cc82c5ccbcf3ecfab74b7c68666765cfc93fe96c
gutssnv <decryptor69@gmail.com>
2026-05-27 14:18:11 +0000

    X6882: Welcome YAAP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X6882.mk
+    $(LOCAL_DIR)/yaap_X6882.mk
--- yaap_X6882.mk
+++ yaap_X6882.mk
@@ -8,26 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Inherit from X6882 device
+# Inherit from device makefile
 $(call inherit-product, device/infinix/X6882/device.mk)
 
+# Inherit some common YAAP stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+
 BOARD_VENDOR := Infinix
-PRODUCT_NAME := lineage_X6882
+PRODUCT_NAME := yaap_X6882
 PRODUCT_DEVICE := X6882
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6882
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
-PRODUCT_SYSTEM_NAME := X6882-OP
-PRODUCT_SYSTEM_DEVICE := X6882
 
-BUILD_FINGERPRINT := Infinix/X6882-OP/Infinix-X6882:14/UP1A.231005.007/260117V1572:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceName=X6882 \
+    BuildFingerprint=Infinix/X6882-OP/Infinix-X6882:14/UP1A.231005.007/260117V1572:user/release-keys
 
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
-
+TARGET_BOARD_PLATFORM := mt6789
