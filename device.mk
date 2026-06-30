9fa50129557f4a5356a972d960354240ba282751
gutssnv <decryptor69@gmail.com>
2026-06-30 23:59:02 +0000

    [revert] X6882: revert Enable userspace reboots
    This reverts commit 357becb5115c044d310f0964a57733127c56d2df.

--- device.mk
+++ device.mk
@@ -47,9 +47,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
-# Allow userspace reboots
-$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
-
 # Audio
 $(call soong_config_set,android_hardware_audio,run_64bit,true)
 $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
