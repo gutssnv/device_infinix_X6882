499f19838449ffaa02d2f97ea7b3f959f30b4d7e
gutssnv <decryptor69@gmail.com>
2026-07-09 19:04:45 +0700

    X6882: welcome YAAP

--- yaap_X6882.mk
+++ yaap_X6882.mk
@@ -7,15 +7,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common YAAP stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Inherit from X6882 device
 $(call inherit-product, device/infinix/X6882/device.mk)
 
 BOARD_VENDOR := Infinix
-PRODUCT_NAME := lineage_X6882
+PRODUCT_NAME := yaap_X6882
 PRODUCT_DEVICE := X6882
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
@@ -27,6 +26,5 @@ PRODUCT_SYSTEM_DEVICE := X6882
 
 BUILD_FINGERPRINT := Infinix/X6882-OP/Infinix-X6882:14/UP1A.231005.007/260117V1572:user/release-keys
 
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
 
